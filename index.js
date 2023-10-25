4c420576330cbd6b2c3d43ae70a57c3554ffefcf
tyler kim <yoonsoo.kim11@gmail.com>
2023-10-25 09:49:29 -0400

    adds package-lock.json and modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Tyler Kim',
+    streetAddress: '42628 Burbank ter',
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return { ...employee,
+    [key]: value,
+};
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+     employee[key] = value;
+     return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    const newEmployee = {...employee };
+    delete newEmployee[key];
+    return newEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
